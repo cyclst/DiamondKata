12b64b3d377e85d47faf5122156aacf99d358aa3
Richard Lang <richard.lang@puregym.com>
2023-07-04 11:06:46 +0100

    Make Diamond_ShouldDisplayAsSingleCharacter_WhenInitialisedWithA test pass

--- src/DiamondKata/Diamond.cs
+++ src/DiamondKata/Diamond.cs
@@ -2,8 +2,15 @@
 
 public class Diamond
 {
+    private char _letter;
+
     public Diamond(char letter)
     {
+        _letter = letter;
+    }
 
+    public override string ToString()
+    {
+        return _letter.ToString();
     }
 }
